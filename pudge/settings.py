1b3a30ae92ae7d3edf69dce824d1927d623ea5fd
root <root@web.asia-east2-a.c.wired-armor-254514.internal>
2019-12-04 12:10:31 +0000

    feat(init): blog module basic framework, it works!

--- pudge/settings.py
+++ pudge/settings.py
@@ -156,38 +156,50 @@ REST_FRAMEWORK = {
 MEDIA_ROOT = os.path.join(BASE_DIR, 'storage/uploads')
 MEDIA_URL = '/media/'
 
+# mdeditor markdown编辑器配置
 MDEDITOR_CONFIGS = {
-    'default': {
-        'width': '90% ',  # Custom edit box width
-        'heigth': 500,  # Custom edit box height
-        'toolbar': ["undo", "redo", "|",
-                    "bold", "del", "italic", "quote", "ucwords", "uppercase",
-                    "lowercase", "|",
-                    "h1", "h2", "h3", "h5", "h6", "|",
-                    "list-ul", "list-ol", "hr", "|",
-                    "link", "reference-link", "image", "code",
-                    "preformatted-text", "code-block", "table", "datetime"
-                                                                "emoji",
-                    "html-entities", "pagebreak", "goto-line", "|",
-                    "help", "info",
-                    "||", "preview", "watch", "fullscreen"],
-    # custom edit box toolbar
-        'upload_image_formats': ["jpg", "jpeg", "gif", "png", "bmp", "webp"],
-    # image upload format type
-        'image_folder': 'editor',  # image save the folder name
-        'theme': 'default',  # edit box theme, dark / default
-        'preview_theme': 'default',  # Preview area theme, dark / default
-        'editor_theme': 'default',  # edit area theme, pastel-on-dark / default
-        'toolbar_autofixed': True,  # Whether the toolbar capitals
-        'search_replace': True,  # Whether to open the search for replacement
-        'emoji': True,  # whether to open the expression function
-        'tex': True,  # whether to open the tex chart function
-        'flow_chart': True,  # whether to open the flow chart function
-        'sequence': True,  # Whether to open the sequence diagram function
-        'watch': True,  # Live preview
-        'lineWrapping': False,  # lineWrapping
-        'lineNumbers': False  # lineNumbers
-    }
+    'default':{
+    'width': '90%',  # 自定义编辑框宽度
+    'heigth': 500,   # 自定义编辑框高度
+    'toolbar': ["undo", "redo", "|",
+                "bold", "del", "italic", "quote", "ucwords", "uppercase", "lowercase", "|",
+                "h1", "h2", "h3", "h5", "h6", "|",
+                "list-ul", "list-ol", "hr", "|",
+                "link", "reference-link", "image", "code", "preformatted-text", "code-block", "table", "datetime",
+                "emoji", "html-entities", "pagebreak", "goto-line", "|",
+                "help", "info",
+                "||", "preview", "watch", "fullscreen"],  # 自定义编辑框工具栏
+    'upload_image_formats': ["jpg", "jpeg", "gif", "png", "bmp", "webp"],  # 图片上传格式类型
+    'image_floder': 'editor',  # 图片保存文件夹名称
+    'theme': 'default',  # 编辑框主题 ，dark / default
+    'preview_theme': 'default',  # 预览区域主题， dark / default
+    'editor_theme': 'default',  # edit区域主题，pastel-on-dark / default
+    'toolbar_autofixed': True,  # 工具栏是否吸顶
+    'search_replace': True,  # 是否开启查找替换
+    'emoji': True,  # 是否开启表情功能
+    'tex': True,  # 是否开启 tex 图表功能
+    'flow_chart': True,  # 是否开启流程图功能
+    'sequence': True  # 是否开启序列图功能
+    },
+
+    'form_config': {
+        'width': '70%',  # 自定义编辑框宽度
+        'heigth': 500,   # 自定义编辑框高度
+        'toolbar': ["undo", "redo", "|", "link", "reference-link", "image", "code", "preformatted-text", "code-block", "table",
+                    "emoji",  "|",
+                    "help", "info", "preview", "watch", "fullscreen"],  # 自定义编辑框工具栏
+        'upload_image_formats': ["jpg", "jpeg", "gif", "png", "bmp", "webp"],  # 图片上传格式类型
+        'image_floder': 'editor',  # 图片保存文件夹名称
+        'theme': 'dark',  # 编辑框主题 ，dark / default
+        'preview_theme': 'default',  # 预览区域主题， dark / default
+        'editor_theme': 'default',  # edit区域主题，pastel-on-dark / default
+        'toolbar_autofixed': True,  # 工具栏是否吸顶
+        'search_replace': True,  # 是否开启查找替换
+        'emoji': True,  # 是否开启表情功能
+        'tex': True,  # 是否开启 tex 图表功能
+        'flow_chart': True,  # 是否开启流程图功能
+        'sequence': True  # 是否开启序列图功能
+        },
 
 }
 
