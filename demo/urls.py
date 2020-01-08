c6eb51f350eed48f13c374c9c2630c43fcb0cbb2
zhuqin <zhuqin@grandomics.com>
2020-01-08 16:01:44 +0800

    feat(demo): Added demo app

--- demo/urls.py
+++ demo/urls.py
@@ -6,4 +6,9 @@ router = routers.SimpleRouter(trailing_slash=False)
 router.register(r'basic-demo', BasicDemoViewSet)
 router.register(r'bootstrap4-html-demo', Bootstrap4DemoViewSet)
 router.register(r'html-demo', HtmlDemoViewSet)
+router.register(r'md-demo', MDDemoViewSet)
 urlpatterns = router.urls
+
+
+import mistune
+mistune.Renderer()
