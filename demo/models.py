83835468767ea98f39a27a60b0b342f894a0f89f
zhuqin <zhuqin@grandomics.com>
2020-04-27 17:33:18 +0800

    ugly 2

--- demo/models.py
+++ demo/models.py
@@ -11,7 +11,7 @@ class Test(CoreModel):
 class BasicDemo(CoreModel):
     name = models.CharField(max_length=512, verbose_name='测试字段')
     test = models.BooleanField(default=False)
-    test_fk = models.ForeignKey(Test, related_name='basicdemo', verbose_name='测试用')
+    test_fk = models.ForeignKey(Test, related_name='basicdemo', on_delete=models.CASCADE, verbose_name='测试用')
 
     def save(self, force_insert=False, force_update=False, using=None,
              update_fields=None):
