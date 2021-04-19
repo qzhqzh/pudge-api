4287f3d1658fcadf36818ff39d0de7c1fe98782f
zhuqin <zhuqin@grandomics.com>
2021-04-19 10:23:45 +0800

    feat(kbms): 新增 editorjs 来创建 kbms 文档

--- kbms/renders.py
+++ kbms/renders.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 
+
 def editor(request):
     return render(request, 'editor.html', {'page': 'Editor'})
 
 
 def create_article(request):
     return render(request, 'kbms/create-article.html', {'page': 'Editor'})
+
+
+def list_article(request):
+    return render(request, 'kbms/list-article.html', {'page': 'Editor'})
